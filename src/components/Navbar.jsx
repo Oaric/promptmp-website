920c33a53936852182ea837645af37d246215fbc
Oaric <126637043+Oaric@users.noreply.github.com>
2025-11-29 16:02:40 -0500

    Add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white p-4">
+      <h1 className="font-bold">PromptMP</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
