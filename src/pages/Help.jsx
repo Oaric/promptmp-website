e9798fb04f9076eba782af086913b9eabe3e1030
Oaric <126637043+Oaric@users.noreply.github.com>
2025-11-29 16:03:14 -0500

    Create Help.jsx component

---
+++ src/pages/Help.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Help() {
+  return (
+    <section className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Help</h1>
+      <p>How can we help you?</p>
+    </section>
+  );
+}
+
+export default Help;
