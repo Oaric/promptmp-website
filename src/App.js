b1993acbdb7dd1c25f0b0ca9f8ec9ce81169a968
Oaric <126637043+Oaric@users.noreply.github.com>
2025-11-29 16:07:56 -0500

    Implement routing in App component
    Refactor App component to use React Router for navigation.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Features from './pages/Features';
+import Help from './pages/Help';
+import Account from './pages/Account';
+import Shop from './pages/Shop';
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <Router>
       <Navbar />
-      <main className="flex-grow">
-        <Home />
-      </main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/features" element={<Features />} />
+        <Route path="/help" element={<Help />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
