43a4c3dfff4eb7fdb2f9d96257e152b82c020d99
Oaric <126637043+Oaric@users.noreply.github.com>
2025-11-29 16:01:50 -0500

    Created Account component file

---
+++ src/pages/Account.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Account() {
+  return (
+    <section className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Account</h1>
+      <p>Manage your account here.</p>
+    </section>
+  );
+}
+
+export default Account;
